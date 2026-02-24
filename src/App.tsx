6624908067e808a64103f7ec3f1f2ee748b37b88
Harry7986 <emarh.harry.code@gmail.com>
2026-02-24 12:10:33 +0100

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import './App.css'
+import { ModalDemo } from './components/ModalDemo';
 
 function App() {
-
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <ModalDemo />;
 }
 
-export default App
+export default App;
